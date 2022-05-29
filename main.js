d3046ceda3116e48ec124bbe38bed8dc3cb33560
Chris Moore <57775710+chrism-48@users.noreply.github.com>
2022-05-29 18:59:24 -0400

    added data

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({ // pass in options object here inside function and we are using
+    data() { // shorthand for function (createApp function () {}) === (createApp() {})
+        return {
+            product: 'Socks',
+            description: 'red'
+        }
+    }
+})
